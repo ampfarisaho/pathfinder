c28ca91036c04ab81dbbe5d2341c04872eb975e2
Ampfarisaho Maphangwa <ampfarisaho@zulzi.com>
2026-02-21 13:40:12 +0200

    Add material icons lib

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.hilt.navigation)
+    implementation(libs.androidx.compose.material.icons.core)
     debugImplementation(libs.androidx.compose.ui.tooling)
 
     implementation(libs.nav3.ui)
